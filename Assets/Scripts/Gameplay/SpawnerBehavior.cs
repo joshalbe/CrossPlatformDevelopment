1a6ed0d1199cb5f3b475242869dce48f5b0f02ae
Joshua Albe <joshjalbe@gmail.com>
2021-04-19 15:56:02 -0500

    Initial commit

---
+++ Assets/Scripts/Gameplay/SpawnerBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _spawnedEntity;
+    [SerializeField]
+    private float _spawnTimer;
+    [SerializeField]
+    private bool _canSpawn;
+    [SerializeField]
+    private GameObject _targetEntity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEntities());
+    }
+
+    //Spawns Entities as long as it can
+    public IEnumerator SpawnEntities()
+    {
+        while (_canSpawn)
+        {
+            //Create a new enemy in the scene
+            GameObject spawnedEnemy = Instantiate(_spawnedEntity, transform.position, transform.rotation);
+            //Set the enemy target to be the target the spawner was given
+            spawnedEnemy.GetComponent<MovementEnemy>().Target = _targetEntity;
+            //Pause for the given time in seconds before resuming the function
+            yield return new WaitForSeconds(_spawnTimer);
+        }
+    }
+}
